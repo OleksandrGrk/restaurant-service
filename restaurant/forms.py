fd14850ab710621cc5663cfefb573f5738a5adfb
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-10-27 12:46:06 +0300

    add creation forms for dish, dish types and cooks

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from restaurant.models import Cooker
+from restaurant.models import Cooker, DishType, Dish
 
 
 class CookerCreationForm(UserCreationForm):
@@ -10,4 +11,19 @@ class CookerCreationForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ("years_of_experience",)
 
 
+class DishCreationForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
 
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class DishTypeCreationForm(forms.ModelForm):
+    class Meta:
+        model = DishType
+        fields = "__all__"
