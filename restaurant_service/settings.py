d9019779a75d82aeba4fd9247868b940d4b8e212
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-11-15 14:41:53 +0200

    add .env file with SECRET_KEY and dotenv to  setting.py

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os.path
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+SECRET_KEY = str(os.getenv("SECR"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=-m$4_u$(&uk8k@)dvxi1$5ctu&$#qv1my1e1!@5ll5_z5ykr%"
+# SECRET_KEY = "django-insecure-=-m$4_u$(&uk8k@)dvxi1$5ctu&$#qv1my1e1!@5ll5_z5ykr%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
